2259dd11f95eb3dfcd2aa8c0e31018a684b05916
Jean Alvarez <225872103+kensulite@users.noreply.github.com>
2025-08-21 16:41:14 -0500

    Create Player factory

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 const Gameboard = () => {
     const board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 }
+
+const Player = (name, piece) => {
+    const getName = () => name;
+    const getPiece = () => piece;
+    return { getName, getPiece };
+}
