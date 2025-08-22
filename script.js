0704bb5362e77b7890a627d5be66dca3e712fc4b
Jean Alvarez <225872103+kensulite@users.noreply.github.com>
2025-08-22 08:41:51 -0500

    Create Elements module

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ const createPlayer = (name, piece) => {
 const createGame = () => {
     const board = Gameboard();
 }
+
+const DisplayController = (function () {
+    const grid = document.querySelector(".grid");
+
+    const fillGrid = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const gridButton = ElementCreator.createGridButton(i, j);
+                grid.append(gridButton);
+            }
+        }
+    }
+
+    return { fillGrid }
+})();
+
+const ElementCreator = (function () {
+    const createGridButton = (row, col) => {
+        const button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("data-col", col);
+        button.setAttribute("data-row", row);
+        return button;
+    }
+
+    return { createGridButton }
+})();
+
+DisplayController.fillGrid();
